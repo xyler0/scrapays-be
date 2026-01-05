f4bfca58fc3b4c0ae989433afb04aa55558a268c
xyler0 <harjaserridwan@gmail.com>
2026-01-05 16:56:16 +0100

    fix(graphql): register missing import of BookResolver in book Module

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './book.entity';
 import { BookService } from './book.service';
+import { BookResolver } from './book.resolver';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Book])],
-  providers: [BookService, ],
+  providers: [BookService, BookResolver],
 })
 export class BookModule {}
